1244cb74626804607678bca47b1e6afa66c25997
acionca <alexandre.cionca@gmail.com>
2026-02-10 17:54:09 +0100

    ref: dir structure

---
+++ bimodularity/__init__.py
@@ -0,0 +1,12 @@
+"""Bimodularity module"""
+
+__version__ = "0.1.0"
+__author__ = "Alexandre Cionca"
+
+# Import main classes/functions for easier access
+# from .core import SomeClass, some_function
+
+__all__ = [
+    # "SomeClass",
+    # "some_function",
+]
